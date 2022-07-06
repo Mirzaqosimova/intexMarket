b5607fa2ac2bc207b1d5e50b84266cb499d34f5c
hp <mirzaqosimovafarzona@gmail.com>
2022-07-06 13:46:04 +0500

    go

---
+++ Intex/src/main/java/com/intex/intex/IntexApplication.java
@@ -0,0 +1,13 @@
+package com.intex.intex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntexApplication.class, args);
+    }
+
+}
