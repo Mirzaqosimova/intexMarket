610fb2c58edacdbd0fce1278f56fc2abb3246085
Ozoda Yuldoshova <yuldoshovayou@gmail.com>
2022-07-07 14:56:47 +0500

    commit

--- Intex/src/main/java/com/example/index_market/dto/product/ProductDto.java
+++ Intex/src/main/java/com/example/index_market/dto/product/ProductDto.java
@@ -1,14 +1,10 @@
 package com.example.index_market.dto.product;
 
 import com.example.index_market.dto.GenericDto;
-import com.example.index_market.entity.product.Category;
-import com.example.index_market.entity.product.Frame;
-import com.example.index_market.enums.product.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
--- Intex/src/main/java/com/example/index_market/mapper/product/ProductMapImpl.java
+++ Intex/src/main/java/com/example/index_market/mapper/product/ProductMapImpl.java
@@ -3,18 +3,22 @@ package com.example.index_market.mapper.product;
 import com.example.index_market.dto.product.ProductCreateDto;
 import com.example.index_market.dto.product.ProductDto;
 import com.example.index_market.dto.product.ProductUpdateDto;
+import com.example.index_market.entity.product.Category;
+import com.example.index_market.entity.product.Detail;
 import com.example.index_market.entity.product.Product;
+import com.example.index_market.enums.product.Status;
 import com.example.index_market.mapper.BaseMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
-public class ProductMapImpl implements   BaseMapper<Product,
+public class ProductMapImpl implements BaseMapper<Product,
         ProductDto,
         ProductCreateDto,
         ProductUpdateDto> {
     @Override
-    public  ProductDto toDto(Product product) {
+    public ProductDto toDto(Product product) {
         ProductDto productDto = new ProductDto();
         productDto.setId(product.getId());
         return productDto;
@@ -34,6 +38,28 @@ public class ProductMapImpl implements   BaseMapper<Product,
     @Override
     public Product fromCreateDto(ProductCreateDto productCreateDto) {
         return null;
+    }
+
+    public Product fromCreateDtoToProduct(ProductCreateDto productCreateDto,
+                                          Category category,
+                                          Status status,
+                                          List<Detail> detailList) {
+
+        Product product = new Product(
+                productCreateDto.getCount(),
+                productCreateDto.getPrice(),
+                productCreateDto.getDescription(),
+                productCreateDto.getDisPrice(),
+                productCreateDto.getSize(),
+                productCreateDto.getHeight(),
+                productCreateDto.getFrameUz(),
+                productCreateDto.getFrameRu(),
+                category,
+                status,
+                productCreateDto.getImageUrl(),
+                detailList
+        );
+        return product;
     }
 
     @Override
